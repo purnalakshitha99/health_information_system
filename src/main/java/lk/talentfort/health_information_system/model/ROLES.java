b8140412d97b8a332e41cbf229e88ae7479e25de
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-12-02 09:27:07 +0530

    add new roles for roles enum

--- src/main/java/lk/talentfort/health_information_system/model/ROLES.java
+++ src/main/java/lk/talentfort/health_information_system/model/ROLES.java
@@ -3,4 +3,8 @@ package lk.talentfort.health_information_system.model;
 public enum ROLES {
     ROLE_USER,
     ROLE_ADMIN,
+    ROLE_DOCTOR_LEVEL1,
+    ROLE_DOCTOR_LEVEL2,
+    ROLE_NURSE_LEVEL1,
+    ROLE_NURSE_LEVEL2
 }
