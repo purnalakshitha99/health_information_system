03c84916d30ce635d341ba4992df5f4f2c5f4d79
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-12-10 21:38:58 +0530

    add report_chemistry repository

---
+++ src/main/java/lk/talentfort/health_information_system/repository/ReportChemistryRepository.java
@@ -0,0 +1,7 @@
+package lk.talentfort.health_information_system.repository;
+
+import lk.talentfort.health_information_system.model.ReportChemistry;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportChemistryRepository extends JpaRepository<ReportChemistry,Long> {
+}
