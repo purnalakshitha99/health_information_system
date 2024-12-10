48223848dccc6905bb02becdfe26d66481a08612
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-12-10 16:14:25 +0530

    added new column for report type

--- src/main/java/lk/talentfort/health_information_system/controller/request/ReportTypeRq.java
+++ src/main/java/lk/talentfort/health_information_system/controller/request/ReportTypeRq.java
@@ -6,5 +6,6 @@ import lombok.Data;
 public class ReportTypeRq {
 
     private String reportName;
+    private Float price;
 
 }
--- src/main/java/lk/talentfort/health_information_system/controller/response/ReportTypeResponse.java
+++ src/main/java/lk/talentfort/health_information_system/controller/response/ReportTypeResponse.java
@@ -6,4 +6,5 @@ import lombok.Data;
 public class ReportTypeResponse {
 
     private String reportName;
+    private Float price;
 }
